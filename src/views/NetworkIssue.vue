03bd9a7eb44092a282f174597482195c98f2718b
Chris <bywon97@gmail.com>
2019-09-27 15:58:19 -0400

    VUE APP

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Ruh-roh shaggy...</h1>
+    <h3>It looks like you're experiencing some networking issues, Please click and try again.</h3>
+    <router-link :to="{ name: 'event-list'}">Or go back to the Home Page</router-link>
+  </div>
+</template>
